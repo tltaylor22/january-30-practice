221e305c7474e8db82f92d24963274f412045824
Tim Taylor <ttaylor@clarion.edu>
2017-01-31 13:01:52 -0500

    Review of code

--- jan2_30.rb
+++ jan2_30.rb
@@ -6,12 +6,28 @@ def off_by_one(winnum, ticket_num)
 	ticket_num.length.times do
 
 	if ticket_num[index_position] == winnum[index_position]
-		matches = matches + 1
+		matches = matches + 1 # or matches += 1
 	end
 
 	index_position += 1
-end
+	end
 
-winnum.length - matches == 1
+	if winnum.length - matches == 1
+		true
+	else
+		false
+	end
 
+end	
+
+def returning_function(winnum, ticket_num)
+	array = []
+	
+	ticket_num.each do |ticket_num|
+		
+		if off_by_one(ticket_num, winnum)
+			array << ticket_num
+		end	
+	end
+	array
 end	
--- test2_jan_30.rb
+++ test2_jan_30.rb
@@ -1,7 +1,7 @@
 require 'minitest/autorun'
 require_relative 'jan2_30.rb' 
 
-class OffByOne < Minitest::Test 
+class TestOffByOne < Minitest::Test 
 
 # Part 2: Write a function that takes two numbers as strings, and returns true if they are off by one digit. eg if we pass '1234' and '1224' then the function should return true. '1234' and '1325' should return false.
 	def test_false_if_numbers_are_not_off_by_one
@@ -26,6 +26,19 @@ class OffByOne < Minitest::Test
 	end
 end	
 
-
+class TestReturnRaffle < Minitest::Test 
 
 # Part 3: Write a function that takes a number (our ticket number) and an array of numbers (the winning numbers from all the draws). To start with, just restrict to all 4 digit numbers. Having the numbers as strings may make it easier to solve. The function should return an array of all the winning numbers that are one digit off from our ticket number. eg our_number = '1234' and winning numbers = ['9999', '5678', '1235', '1134', '1344'] should return ['1235', '1134']
+	def test_return_array
+		winnum = '1234'
+		ticket_num = ['5678', '9999', '5656']
+		assert_equal([], returning_function(winnum, ticket_num))
+	end
+
+	def test_return_array_off1
+		winnum = '1234'
+		ticket_num = ['3234', '5555', '6789']
+		assert_equal(['3234'], returning_function(winnum, ticket_num))
+	end
+	
+end			
